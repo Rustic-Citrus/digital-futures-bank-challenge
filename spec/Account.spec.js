a342e1d5d550e920e1601432d7525bfa68490d8c
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-25 14:54:11 +0100

    test: it should raise an error if the user tries to deposit a negative amount

--- spec/Account.spec.js
+++ spec/Account.spec.js
@@ -9,13 +9,19 @@ describe("Account Class Tests, ", () => {
 
   afterEach(() => {
     testAccount = undefined;
-  })
+  });
 
   describe("Account.deposit() Tests, ", () => {
     it("should update the balance of the account if an amount is deposited.", () => {
       testAccount.deposit(100);
 
       expect(testAccount.getBalance()).toBe(100);
-    })
-  })
-})
+    });
+
+    it("should raise an error if the user tries to deposit a negative amount.", () => {
+      testAccount.deposit(-50);
+
+      expect(testAccount.deposit).toThrowError(Error, "Cannot deposit a negative amount.");
+    });
+  });
+});
