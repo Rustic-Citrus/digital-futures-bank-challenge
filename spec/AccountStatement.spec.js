bf226aebc94319fc00e5ba67033202c04b3f7d9e
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-26 15:27:44 +0100

    test: updated console log test
    fix: added functionality for AccountStatement.printStatement()

--- spec/AccountStatement.spec.js
+++ spec/AccountStatement.spec.js
@@ -13,19 +13,21 @@ describe("AccountStatement Class Tests,", () => {
   });
 
   it("should show the statement in the console in the correct format", () => {
-    const expectedLogs = [
-      "date       || credit  || debit  || balance",
-      "14/01/2012 ||         || 500.00 || 2500.00",
-      "13/01/2012 || 2000.00 ||        || 3000.00",
-      "10/01/2012 || 1000.00 ||        || 1000.00"
+    const expectedLines = [
+      "date       || credit  || debit  || balance ",
+      "14/01/2012 ||         || 500.00 || 2500.00 ",
+      "13/01/2012 || 2000.00 ||        || 3000.00 ",
+      "10/01/2012 || 1000.00 ||        || 1000.00 "
     ];
 
     spyOn(console, "log");
 
     testAccountStatement.printStatement();
 
-    expectedLogs.forEach(log => {
-      expect(console.log).toHaveBeenCalledWith(log);
+    expect(console.log.calls.count()).toBe(4);
+
+    expectedLines.forEach((string, i) => {
+      expect(console.log.calls.argsFor(i)).toEqual(string);
     });
   });
 });
--- src/AccountStatement.js
+++ src/AccountStatement.js
@@ -1,3 +1,99 @@
 export default class AccountStatement {
-  
+  #account;
+
+  constructor(accountObject) {
+    this.#account = accountObject;
+  }
+
+  getMaxStringLength(property) {
+    const transactions = this.#account.getTransactions();
+
+    let maxStringLength = transactions[0][property].toFixed(2).length;
+
+    transactions.forEach(transaction => {
+      if (transaction[property].toFixed(2).length > maxStringLength) maxStringLength = transaction[property].toFixed(2).length;
+    });
+
+    return maxStringLength;
+  }
+
+  calculateColWidths(propertyName) {
+    const maxLengthWithSpaces = this.getMaxStringLength(propertyName) + 2;
+    const propertyNameWithSpaces = propertyName.length + 2;
+
+    if (maxLengthWithSpaces > propertyNameWithSpaces) {
+      return maxLengthWithSpaces;
+    } else {
+      return propertyNameWithSpaces;
+    }
+  }
+
+  createCols(colNames) {
+    const cols = new Array();
+
+    colNames.forEach(name => {
+      cols.push({ name, width: this.calculateColWidths(name) });
+    });
+
+    return cols;
+  }
+
+  generateHeaderLine(colNames, cols) {
+    const cells = [ "date       ||" ];
+
+    colNames.forEach(colName => {
+      const findFunc = (col) => {
+        return colName === col.name;
+      };
+
+      const width = cols.find(findFunc).width;
+
+      const cell = colName != "balance" ? ` ${colName} `.padEnd(width, " ") + "||" : ` ${colName}`.padEnd(width, " ");
+
+      cells.push(cell);
+    });
+
+    return cells.join("");
+  }
+
+  generateBodyLine(cols, transaction, colNames) {
+    const cells = [ `${transaction.date} ||` ];
+
+    colNames.forEach(colName => {
+      const findFunc = (col) => {
+        return colName === col.name;
+      };
+
+      const width = cols.find(findFunc).width;
+      const valueString = transaction[colName] != 0 ? transaction[colName].toFixed(2) : "";
+
+      const cell = colName != "balance" ? ` ${valueString} `.padEnd(width, " ") + "||" : ` ${valueString}`.padEnd(width, " ");
+
+      cells.push(cell);
+    });
+
+    return cells.join("");
+  }
+
+  generateLines() {
+    const colNames = [ "credit", "debit", "balance" ];
+    const cols = this.createCols(colNames);
+    const transactions = this.#account.getTransactions().reverse();
+
+    const lines = [ this.generateHeaderLine(colNames, cols) ];
+
+    transactions.forEach(transaction => {
+      lines.push(this.generateBodyLine(cols, transaction, colNames));
+    });
+    
+    return lines;
+  }
+
+  printStatement() {
+    const lines = this.generateLines();
+    
+    lines.forEach(line => {
+      console.log(line);
+    });
+  }
 }
