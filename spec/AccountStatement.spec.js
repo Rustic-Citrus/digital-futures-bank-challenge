71a4b30235f6ba4e48d97a0e8447618680ed9b5a
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-26 13:35:28 +0100

    chore: added AccountStatement class
    test: it should show the statement in the console in the correct format

---
+++ spec/AccountStatement.spec.js
@@ -0,0 +1,31 @@
+import AccountStatement from "../src/AccountStatement.js";
+import Account from "../src/Account.js";
+
+describe("AccountStatement Class Tests,", () => {
+  let testAccountStatement;
+
+  beforeEach(() => {
+    const testAccount = new Account();
+    testAccount.deposit(1000, "10/01/2012");
+    testAccount.deposit(2000, "13/01/2012");
+    testAccount.withdraw(500, "14/01/2012");
+    testAccountStatement = new AccountStatement(testAccount);
+  });
+
+  it("should show the statement in the console in the correct format", () => {
+    const expectedLogs = [
+      "date       || credit  || debit  || balance",
+      "14/01/2012 ||         || 500.00 || 2500.00",
+      "13/01/2012 || 2000.00 ||        || 3000.00",
+      "10/01/2012 || 1000.00 ||        || 1000.00"
+    ];
+
+    spyOn(console, "log");
+
+    testAccountStatement.printStatement();
+
+    expectedLogs.forEach(log => {
+      expect(console.log).toHaveBeenCalledWith(log);
+    });
+  });
+});
