3ac6afdcab0ba4da564b5972685e6742391fdb63
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-26 16:54:56 +0100

    test: fixed test conditions for colour check

--- spec/AccountStatement.spec.js
+++ spec/AccountStatement.spec.js
@@ -23,9 +23,9 @@ describe("AccountStatement Class Tests,", () => {
   it("should show the correct information, formatted correctly, with debits and negative balances in red and credits and positive balances in green", () => {
     const expectedLines = [
       "date       || credit  || debit  || balance ",
-      "14/01/2012 ||         || \x1b[31m500.00\x1b[0m || 2500.00 ",
-      "13/01/2012 || \x1b[32m2000.00\x1b[0m ||        || 3000.00 ",
-      "10/01/2012 || \x1b[32m1000.00\x1b[0m ||        || 1000.00 "
+      "14/01/2012 ||         || \x1b[31m500.00\x1b[0m || \x1b[32m2500.00\x1b[0m ",
+      "13/01/2012 || \x1b[32m2000.00\x1b[0m ||        || \x1b[32m3000.00\x1b[0m ",
+      "10/01/2012 || \x1b[32m1000.00\x1b[0m ||        || \x1b[32m1000.00\x1b[0m "
     ];
 
     spyOn(console, "log");
