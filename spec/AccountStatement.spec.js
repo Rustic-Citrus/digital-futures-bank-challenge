6314a06f0efc7eb5256fe4625e9eac274f316f55
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-26 16:17:51 +0100

    test: updated tests for specificity

--- spec/AccountStatement.spec.js
+++ spec/AccountStatement.spec.js
@@ -12,19 +12,25 @@ describe("AccountStatement Class Tests,", () => {
     testAccountStatement = new AccountStatement(testAccount);
   });
 
-  it("should show the statement in the console in the correct format", () => {
-    const expectedLines = [
-      "date       || credit  || debit  || balance ",
-      "14/01/2012 ||         || 500.00 || 2500.00 ",
-      "13/01/2012 || 2000.00 ||        || 3000.00 ",
-      "10/01/2012 || 1000.00 ||        || 1000.00 "
-    ];
-
+  it("should display the statement across the correct number of lines", () => {
     spyOn(console, "log");
 
     testAccountStatement.printStatement();
 
     expect(console.log.calls.count()).toBe(4);
+  });
+
+  it("should show the debits in red and the credits in green", () => {
+    const expectedLines = [
+      "date       || credit  || debit  || balance ",
+      "14/01/2012 ||         || \x1b[31m500.00\x1b[0m || 2500.00 ",
+      "13/01/2012 || \x1b[32m2000.00\x1b[0m ||        || 3000.00 ",
+      "10/01/2012 || \x1b[32m1000.00\x1b[0m ||        || 1000.00 "
+    ];
+
+    spyOn(console, "log");
+
+    testAccountStatement.printStatement();
 
     expectedLines.forEach((string, i) => {
       expect(console.log.calls.argsFor(i)[0]).toEqual(string);
