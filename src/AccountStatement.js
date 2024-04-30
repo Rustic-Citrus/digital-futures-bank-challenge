50194b2be94e95302888d2599a3970bac4da88f0
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-04-30 16:27:36 +0100

    refactor: decoupled generateRowFromObject
    docs: added comments to make generateRowFromObject easier to understand

--- src/AccountStatement.js
+++ src/AccountStatement.js
@@ -1,4 +1,4 @@
-import { getMaxStringLength } from "../utils/util.js";
+import { getMaxStringLength, generateRowFromObject } from "../utils/util.js";
 
 export default class AccountStatement {
   #account;
@@ -32,59 +32,13 @@ export default class AccountStatement {
       return columns;
     }
 
-    const generateLine = (columns, columnNames, transaction, i) => {
-      const cells = new Array();
-
-      // Header is generated first.
-      if (i === 0) {
-        cells.push("date       ||");
-
-        columnNames.forEach(columnName => {
-          const width = columns.find(col => columnName === col.name).width;
-    
-          // Balance column is handled differently as it is on the end.
-          const cell = columnName != "balance" ? ` ${columnName} `.padEnd(width, " ") + "||" : ` ${columnName} `.padEnd(width, " ");
-    
-          cells.push(cell);
-        });
-
-      // Then the body is generated.
-      } else {
-        cells.push(`${transaction.date} ||`);
-
-        columnNames.forEach(columnName => {
-          const width = columns.find(col => columnName === col.name).width;
-          let valueString;
-          
-          if (columnName === "credit" && transaction[columnName] != 0) {
-            valueString = `\x1b[32m${transaction[columnName].toFixed(2)}\x1b[0m`;
-          } else if (columnName === "debit" && transaction[columnName] != 0) {
-            valueString = `\x1b[31m${transaction[columnName].toFixed(2)}\x1b[0m`;
-          } else if (columnName === "balance" && transaction[columnName] > 0) {
-            valueString = `\x1b[32m${transaction[columnName].toFixed(2)}\x1b[0m`;
-          } else if (columnName === "balance" && transaction[columnName] <= 0) {
-            valueString = `\x1b[31m${transaction[columnName].toFixed(2)}\x1b[0m`;
-          } else {
-            valueString = "";
-          }
-    
-          // Again, balance column is handled differently as it is on the end.
-          const cell = columnName != "balance" ? ` ${valueString} `.padEnd(width, " ") + "||" : ` ${valueString} `.padEnd(width, " ");
-    
-          cells.push(cell);
-        });
-      }
-
-      return cells.join("");
-    }
-
     const columnNames = [ "credit", "debit", "balance" ];
     const columns = createColumns(columnNames);
     const transactions = this.#account.getTransactions().reverse(); // As per user specifications.
     const lines = new Array();
 
     for (let i = 0; i <= transactions.length; i++) {
-      lines.push(generateLine(columns, columnNames, transactions[i-1], i));
+      lines.push(generateRowFromObject(columns, columnNames, transactions[i-1], i));
     }
     
     return lines;
--- utils/util.js
+++ utils/util.js
@@ -7,3 +7,59 @@ export const getMaxStringLength = (objectArray, property) => {
 
   return maxStringLength;
 };
+
+export const generateRowFromObject = (columns, columnNames, object, i) => {
+  // Function that generates the rows of the table from the column information and the data provided.
+  const cells = new Array();
+
+  // Header is generated first.
+  if (i === 0) {
+    cells.push("date       ||"); // First cell is always the same in the header.
+
+    columnNames.forEach(columnName => {
+      const width = columns.find(col => columnName === col.name).width;
+
+      // Balance column is formatted differently, since it is on the far-right of the table.
+      const cell = columnName != "balance" ? ` ${columnName} `.padEnd(width, " ") + "||" : ` ${columnName} `.padEnd(width, " ");
+
+      cells.push(cell);
+    });
+
+  // Then the body is generated.
+  } else {
+    cells.push(`${object.date} ||`); // First cell is always the same in the body.
+
+    columnNames.forEach(columnName => {
+      const width = columns.find(col => columnName === col.name).width;
+
+      // Debit values greater than zero and balances less than or equal to zero are shown in red.
+      const conditionsForRed = (columnName === "debit" && object[columnName] != 0) || (columnName === "balance" && object[columnName] <= 0);
+
+      // Credit values greater than zero and balances greater than zero are shown in green.
+      const conditionsForGreen = (columnName === "credit" && object[columnName] != 0) || (columnName === "balance" && object[columnName] > 0);
+
+      let valueString;
+
+      if (conditionsForGreen) {
+
+        valueString = `\x1b[32m${object[columnName].toFixed(2)}\x1b[0m`; // ASCII escape code for green = "\x1b[32m"
+
+      } else if (conditionsForRed) {
+
+        valueString = `\x1b[31m${object[columnName].toFixed(2)}\x1b[0m`; // ASCII escape code for red = "\x1b[31m"
+
+      } else {
+
+        valueString = "";
+
+      }
+
+      // Balance column is formatted differently, since it is on the far-right of the table.
+      const cell = columnName != "balance" ? ` ${valueString} `.padEnd(width, " ") + "||" : ` ${valueString} `.padEnd(width, " ");
+
+      cells.push(cell);
+    });
+  }
+
+  return cells.join("");
+}
